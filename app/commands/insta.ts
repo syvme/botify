faae0677b2b6cbb8b43a4f11b696f4671869e23c
syvme <89515619+syvme@users.noreply.github.com>
2025-06-14 23:37:25 +0300

    Fixes

--- app/commands/insta.ts
+++ app/commands/insta.ts
@@ -1,5 +1,6 @@
 import FormData from 'form-data'
 import { instagramGetUrl } from 'instagram-url-direct'
+import { after } from 'next/server'
 
 export const data = {
   name: 'insta',
@@ -19,35 +20,37 @@ export const data = {
 export const command = async (i: any) => {
   const link = i.data.options[0].value
 
-  try {
-    const result = await instagramGetUrl(link)
+  after(async () => {
+    const result = await instagramGetUrl(link).catch(() => i.reply({ content: 'Provided link is invalid.' }))
     const media = result.media_details
     if (!media) return
 
     const files = await Promise.all(
       media.slice(0, 10).map(async (item: any) => {
         const res = await fetch(item.url)
+        const arrayBuffer = await res.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+        const isVideo = item.type === 'video'
+        if (isVideo && buffer.length > 8 * 1024 * 1024) return { url: item.url, isLargeVideo: true }
         return {
-          buffer: Buffer.from(await res.arrayBuffer()),
-          filename: `processed.${item.type === 'video' ? 'mp4' : 'png'}`,
-          contentType: item.type === 'video' ? 'video/mp4' : 'image/png',
+          buffer,
+          filename: `processed.${isVideo ? 'mp4' : 'png'}`,
+          contentType: isVideo ? 'video/mp4' : 'image/png',
+          isLargeVideo: false,
         }
       })
     )
 
     const formData = new FormData()
     formData.append('payload_json', JSON.stringify({ type: 4, data }))
-    files.forEach((file, idx) => {
-      formData.append(`files[${idx}]`, file.buffer, {
-        filename: file.filename,
-        contentType: file.contentType,
-      })
+
+    files.forEach(async (file, idx) => {
+      if (file.isLargeVideo) return await i.reply({ content: `[${result.post_info.caption || result.post_info.owner_username}](${file.url})` })
+      else formData.append(`files[${idx}]`, file.buffer, { filename: file.filename, contentType: file.contentType })
     })
 
     return await i.reply(formData.getBuffer(), formData.getHeaders())
-  } catch {
-    await i.reply({ content: 'Provided link is invalid.' })
-  }
+  })
 
   return { type: 5 }
 }
