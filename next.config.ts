a64748a065222a1c79c99363f171d558565262a5
syvme <89515619+syvme@users.noreply.github.com>
2026-01-04 02:30:16 +0300

    Legal redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  async redirects() {
+    return [
+      ...['/terms', '/legal', '/legal/terms', '/tos'].map((source) => ({
+        source,
+        destination: 'https://syvme.com/terms',
+        permanent: true,
+      })),
+      ...['privacy', '/legal/privacy', '/legal/privacypolicy', '/privacypolicy'].map((source) => ({
+        source,
+        destination: 'https://syvme.com/privacy',
+        permanent: true,
+      })),
+    ]
+  },
 }
 
 export default nextConfig
